e12fdd83d85183c52495d169aeb56a8c8370d235
Nemo Sbrin <kevspalcop@gmail.com>
2020-12-04 02:26:45 -0500

    Se corrige codigo

--- crearJavaProyecto.sh
+++ crearJavaProyecto.sh
@@ -30,6 +30,7 @@ func_carpetas(){
 	mkdir $1/META-INF
 	echo "   Manifest.mf"
 	func_creandoManifestoSencillo $1/META-INF/Manifest.mf
+	echo "  d( ^^ )b Ya esta listo"
 }
 
 # Empieza el programa
@@ -37,7 +38,7 @@ func_encabezado
 if [ $# -eq 1 ]; then
 	func_carpetas $1
 else
-	echo "(/ >_<)/ No puso nombre al proyecto"	
+	echo "  (/ ò_ó)/ No puso nombre al proyecto"	
 fi
 # fun_carpetas
 # fun_creandoArchivoClases
