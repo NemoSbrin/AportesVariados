9a7b200c03c9b185c3dcf1b2ed88f89e885209c5
Nemo Sbrin <kevspalcop@gmail.com>
2020-12-06 20:53:04 -0500

    Se añade las opciones en minusculas dentro del case

--- ejecutarTSC.sh
+++ ejecutarTSC.sh
@@ -33,62 +33,62 @@ func_TSC(){
 		echo "  Sí, sí existe."
 		echo "Compilando $1"
 		case $2 in
-			ES3)
+			ES3 | es3)
 				echo "    en version $2"
 				ejecutable="$ejecutable $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES5)
+			ES5 | es5)
 				echo "    en version $2"
 				 #--target puede llevar 'es5' pero solo si se ejecuta directamente en la terminal
 				ejecutable="$ejecutable --target es5 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES6)
+			ES6 | es6)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es6 $1"
 				echo ">> $ejecutable $1 <<"
 				$ejecutable
 			;;
-			ES2015)
+			ES2015 | es2015)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es2015 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES2016)
+			ES2016 | es2016)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es2016 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES2017)
+			ES2017 | es2017)
 				echo "    en version $2"				
 				ejecutable="$ejecutable --target es2017 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES2018)
+			ES2018 | es2018)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es2018 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES2019)
+			ES2019 | es2019)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es2019 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ES2020)
+			ES2020 | es2020)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target es2020 $1"
 				echo ">> $ejecutable <<"
 				$ejecutable
 			;;
-			ESNEXT)
+			ESNEXT | esnext)
 				echo "    en version $2"
 				ejecutable="$ejecutable --target esnext $1"
 				echo ">> $ejecutable <<"
