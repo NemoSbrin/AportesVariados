551d0a331a2ccb2e9edf8e3567bd2534680dbd0f
NemoSbrin <kevspalcop@gmail.com>
2021-01-21 23:13:28 -0500

    vericar caso --enCarpeta

--- bat_Linux/crearNCarpetas.sh
+++ bat_Linux/crearNCarpetas.sh
@@ -32,9 +32,39 @@ func_mensaje_PathDeCarpetaVacio(){
 	echo "  (  '_')/ El path de carpeta está vacio"
 }
 
+# Se usa el pwd.
 func_creandoCarpetas(){
+	echo "  <( '_')7 Verificando si hay espacios en blanco en ruta actual"
+	nBlancos=$(func_hayEspacios)
+	# echo " Hay espacios en blancos? $nBlancos"
+	if [ $nBlancos -gt 1 ]; then
+		echo "  (  '-')/ Se cambia el valor de pwd para aceptar espacios"
+		rutaAUsar=$(func_reemplazandoEspacios $nBlancos)
+	# rutaEspaciosAceptados=$? #Esto no funciona
+		echo $rutaAUsar
+	else
+		rutaAUsar=`pwd`
+		echo "  (  'o')b Todo bien"	
+	fi
 	# echo "    cd $1"
 	# echo "  cantidad de capetas $2"
+	echo "  Creando $1 carpetas en $rutaAUsar"
+	cont=1
+	#a futuro poner el nombre de la carpeta
+	while [ $1 -ge $cont ]; do
+		if [ $cont -le 9 ]; then
+			# echo "    mkdir cap0$cont"
+			mkdir cap0$cont
+		else
+			# echo "    mkdir cap$cont"
+			mkdir cap$cont
+		fi
+		cont=$((cont + 1))
+	done
+}
+
+# Se usa el pwd.
+func_creandoCarpetas2(){
 	echo "  Creando $2 carpetas en $1"
 	cont=1
 	#a futuro poner el nombre de la carpeta
@@ -50,7 +80,8 @@ func_creandoCarpetas(){
 	done
 }
 
-#No existe el return, lo que se hace es imprimir el valor que se va a enviar
+# Se verifica que no haya espacios en pwd.
+# No existe el return, lo que se hace es imprimir el valor que se va a enviar
 func_hayEspacios(){
 	# echo "  <( '_')7 Verificando ruta actual: `pwd`"
 	index=$((0))
@@ -65,15 +96,28 @@ func_hayEspacios(){
 }
 
 func_reemplazandoEspacios(){
-	echo "nueva ruta : $1"
-	aux= `sed -i '/s \\ ' {print $1}`
-	echo "nuevo valor : $aux"
-	# return "hola"
+	nuevaRuta=""
+	index=$((0))
+	# echo $1
+	# echo $(($1-1))
+	for v in `pwd`; do
+		# echo "v: $v"
+		if [ $index -eq 0 ];then
+			nuevaRuta=$v
+		fi
+		if [ $index -ge 1 ] && [ $index -le $(($1-1)) ]; then
+			nuevaRuta="$nuevaRuta\\ $v"
+		fi		
+    	index=$((index + 1))
+    	# echo "nuevaRuta: $nuevaRuta"
+    	# echo "index dentro del for: $index"
+	done
+	# echo "nueva Ruta obtenida: $nuevaRuta"
+	echo $nuevaRuta
 }
 
 func_ejecucion(){
 	echo "datos $1 $2 $3"
-	nBlancos=-1
 	case $1 in
 		"--aqui")
 			if [ "$2" = "" ]; then
@@ -82,16 +126,7 @@ func_ejecucion(){
 				if [ $2 -eq 0 ]; then
 					func_mensaje_NumeroCarpetaVacio
 				else
-					nBlancos=$(func_hayEspacios)
-					echo "  <( '_')7 Verificando si hay espacios en blanco en ruta actual"
-					# echo " Hay espacios en blancos? $nBlancos"
-					if [ $nBlancos -gt 1 ]; then
-						echo "cambiar el valor de pwd"
-						# func_creandoCarpetas `pwd` $2
-					else
-						echo "todo bien"
-						# func_creandoCarpetas `pwd` $2
-					fi					
+					func_creandoCarpetas $2					
 				fi
 			fi
 		;;
@@ -105,7 +140,7 @@ func_ejecucion(){
 					if [ $3 -eq 0 ]; then
 						func_mensaje_NumeroCarpetaVacio
 					else
-						func_creandoCarpetas $2 $3
+						func_creandoCarpetas2 $2 $3
 					fi
 				fi
 			fi
