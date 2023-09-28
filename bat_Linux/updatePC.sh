ea0800b6056559c1ea8e1da524ea0521208cdbdf
Kevin Palacios <kevspalcop@gmail.com>
2023-09-28 01:09:51 -0500

    Actualizando en modulos  las distintas opciones: - Se separa las actualizaciones apt y flatpak en funciones -  1er intento de log

--- bat_Linux/updatePC.sh
+++ bat_Linux/updatePC.sh
@@ -4,22 +4,65 @@ clear
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # ::                autor:   Kevin S. Palacios C.                        ::
 # ::                                                                     ::
-# :: fecha de creacion: 2023-09-23     || version: 1.0.0                 ::
+# :: fecha de creacion: 2023-09-23     || version: 1.1.0                 ::
 # :: fecha de modificacion: 2023-09-23 || Quien modifica: Kevin Palacios ::
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+# definicion de funciones
+func_header(){
+	echo ' ----------------------------------- '
+	echo '|    EJECUTANDO LA ACTUALIZACION    |'
+	echo '|    (  ^o^)b        by Kevin       |'
+	echo ' ----------------------------------- '
+}
 
-echo "-->> Se actualiza los programas con apt <<--"
-sudo apt-get update
-echo "--> Programas a actualizar <--"
-sudo apt list --upgradeable
-echo "--> Instalando actualizaciones <--"
-sudo apt-get upgrade -y
-echo "--> Limpiando instalaciones <--"
-sudo apt autoclean autoremove
-echo "-->> Actualizando programas con flatpak <<--"
-echo "--> Listando programas con flatpak <--"
-flatpak list --app
-flatpak update -y
-echo "--> Removiendo paquetes <--"
-flatpak uninstall --unused
+func_updateApt(){
+    echo ' -----------------------------------'
+    echo "|    Actualizando programas con:    |"
+    echo "|                APT                |"
+    echo '-------------------------------------'
+    sudo apt-get update
+    echo ' ---------------------------------- '
+    echo "| Se lista las actualizaciones a   |"
+    echo "| instalar                         |"
+    echo '------------------------------------'
+    sudo apt list --upgradeable
+    echo ' ---------------------------------- '
+    echo "| Instalando las actualizaciones   |"
+    echo '------------------------------------'
+    sudo apt-get upgrade -y
+    echo ' ---------------------------------- '
+    echo "| Limpiando actualizaciones        |"
+    echo "| obsoletas o antiguas             |"
+    echo '------------------------------------'
+    sudo apt autoclean autoremove
+}
+
+func_updateFlatpak(){
+    echo ' -----------------------------------'
+    echo "|    Actualizando programas con:    |"
+    echo "|              FLATPAK              |"
+    echo '-------------------------------------'
+    echo ' ---------------------------------- '
+    echo "| Se lista las actualizaciones a   |"
+    echo "| instalar                         |"
+    echo '------------------------------------'
+    flatpak list --app
+    echo ' ---------------------------------- '
+    echo "| Instalando las actualizaciones   |"
+    echo '------------------------------------'
+    flatpak update -y
+    echo ' ---------------------------------- '
+    echo "| Limpiando actualizaciones        |"
+    echo "| obsoletas o antiguas             |"
+    echo '------------------------------------'
+    flatpak uninstall --unused
+}
+
+
+logsave -a contenido.txt date
+func_header
+#func_updateApt
+
+
+
